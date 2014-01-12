6fb5a8590d1c8eb92e03016d1733f7968eb03b33
naresh11067 <naresh11067@iiitd.ac.in>
2014-01-12 16:05:35 +0000

    Added folder for everyone and copied the basic shivam bhaiya's code in everyone's folder. Do make appropriate changes and do your work in your respective folder after pulling everything :)

---
+++ Srishti/index.php
@@ -0,0 +1,26 @@
+<?php /* Sample page */ ?>
+<html>
+	<head>
+		<title>Welcome here</title>
+		<meta charset="UTF-8"/>
+		<meta name="author" content="Shivam Chaturvedi -- The Smarter Techie"/>
+		<meta name="keywords" content="design for the new blogging platform"/>
+		<script type="text/javascript" src="js/jquery.js"></script>
+		<script type="text/javascript" src="js/main.js"></script>
+		<link rel="stylesheet" href="css/main.css"/>
+	</head>
+	<body>
+		<div id="container">
+			<div id="header">
+				<!-- Head section. Can contain the toolbar or the nav bar if needed or can be merged with the main content area-->
+			</div>
+			<div id="wrapper">
+				<!-- 	wrapper will contain the main article section plus the side bar.
+						So, this in effect wraps the content box and the sidebar. -->
+			</div>
+			<div id="footer">
+				<!-- -->
+			</div>
+		</div>
+	</body>
+</html>
