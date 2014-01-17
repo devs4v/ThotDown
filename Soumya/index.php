a4e3b69c772d60a89166a516161d3eb168e7eaff
Soumya-13106 <soumya13106@iiitd.ac.in>
2014-01-18 02:24:59 +0530

    Learning new things

--- Soumya/index.php
+++ Soumya/index.php
@@ -1,22 +1,59 @@
 <?php /* Sample page */ ?>
 <html>
 	<head>
-		<title>Welcome here</title>
+		<title>Soumyas page</title>
 		<meta charset="UTF-8"/>
 		<meta name="author" content="Shivam Chaturvedi -- The Smarter Techie"/>
 		<meta name="keywords" content="design for the new blogging platform"/>
+		<link rel="stylesheet" href="css/main.css"/>
 		<script type="text/javascript" src="js/jquery.js"></script>
 		<script type="text/javascript" src="js/main.js"></script>
-		<link rel="stylesheet" href="css/main.css"/>
 	</head>
 	<body>
 		<div id="container">
 			<div id="header">
 				<!-- Head section. Can contain the toolbar or the nav bar if needed or can be merged with the main content area-->
+				<div id="navbar">
+					<ul id="menu">
+						<li class="item">
+							<div>Item 1</div>
+							<ul class="submenu">
+								<li class="submenu-item">Submenu1 item1</li>
+								<li class="submenu-item">Submenu1 item2</li>
+							</ul>
+						</li>
+						<li class="item">
+							<div>Item 2</div>
+							<ul class="submenu">
+								<li class="submenu-item">Submenu2 item1</li>
+								<li class="submenu-item">Submenu2 item2</li>
+								<li class="submenu-item">Submenu2 item3</li>
+							</ul>
+						</li>
+						<li class="item">Item 3</li>
+					</ul>
+				</div>
 			</div>
 			<div id="wrapper">
 				<!-- 	wrapper will contain the main article section plus the side bar.
 						So, this in effect wraps the content box and the sidebar. -->
+				<br/><br/><br/>
+				<img src="Books-06.jpg" style="float: left;height: 300px; width: 300px;"/>
+				<img src="Books-06.jpg" style="float: left;height: 300px; width: 300px;"/>
+				<img src="Books-06.jpg" style="float: left;height: 300px; width: 300px;"/>
+				<img src="Books-06.jpg" style="float: left;height: 300px; width: 300px;"/>
+				<img src="Books-06.jpg" style="float: left;height: 300px; width: 300px;"/>
+				<img src="Books-06.jpg" style="float: left;height: 300px; width: 300px;"/>
+				<img src="Books-06.jpg" style="float: left;height: 300px; width: 300px;"/>
+				<img src="Books-06.jpg" style="float: left;height: 300px; width: 300px;"/>
+				<img src="Books-06.jpg" style="float: left;height: 300px; width: 300px;"/>
+				<img src="Books-06.jpg" style="float: left;height: 300px; width: 300px;"/>
+				<img src="Books-06.jpg" style="float: left;height: 300px; width: 300px;"/>
+				<img src="Books-06.jpg" style="float: left;height: 300px; width: 300px;"/>
+				<p style="padding: 0; margin: 0;">
+				This is supposed to be a very long text somewhere that you should not bother to read anyhow. well that doesn't really matter because I will writ it anyway...so please sutt up.This is supposed to be a very long text somewhere that you should not bother to read anyhow. well that doesn't really matter because I will writ it anyway...so please sutt up.This is supposed to be a very long text somewhere that you should not bother to read anyhow. well that doesn't really matter because I will writ it anyway...so please sutt up.This is supposed to be a very long text somewhere that you should not bother to read anyhow. well that doesn't really matter because I will writ it anyway...so please sutt up.This is supposed to be a very long text somewhere that you should not bother to read anyhow. well that doesn't really matter because I will writ it anyway...so please sutt up.This is supposed to be a very long text somewhere that you should not bother to read anyhow. well that doesn't really matter because I will writ it anyway...so please sutt up.This is supposed to be a very long text somewhere that you should not bother to read anyhow. well that doesn't really matter because I will writ it anyway...so please sutt up.This is supposed to be a very long text somewhere that you should not bother to read anyhow. well that doesn't really matter because I will writ it anyway...so please sutt up.This is supposed to be a very long text somewhere that you should not bother to read anyhow. well that doesn't really matter because I will writ it anyway...so please sutt up.This is supposed to be a very long text somewhere that you should not bother to read anyhow. well that doesn't really matter because I will writ it anyway...so please sutt up.This is supposed to be a very long text somewhere that you should not bother to read anyhow. well that doesn't really matter because I will writ it anyway...so please sutt up.This is supposed to be a very long text somewhere that you should not bother to read anyhow. well that doesn't really matter because I will writ it anyway...so please sutt up.This is supposed to be a very long text somewhere that you should not bother to read anyhow. well that doesn't really matter because I will writ it anyway...so please sutt up.This is supposed to be a very long text somewhere that you should not bother to read anyhow. well that doesn't really matter because I will writ it anyway...so please sutt up.This is supposed to be a very long text somewhere that you should not bother to read anyhow. well that doesn't really matter because I will writ it anyway...so please sutt up.This is supposed to be a very long text somewhere that you should not bother to read anyhow. well that doesn't really matter because I will writ it anyway...so please sutt up.This is supposed to be a very long text somewhere that you should not bother to read anyhow. well that doesn't really matter because I will writ it anyway...so please sutt up.This is supposed to be a very long text somewhere that you should not bother to read anyhow. well that doesn't really matter because I will writ it anyway...so please sutt up.This is supposed to be a very long text somewhere that you should not bother to read anyhow. well that doesn't really matter because I will writ it anyway...so please sutt up.This is supposed to be a very long text somewhere that you should not bother to read anyhow. well that doesn't really matter because I will writ it anyway...so please sutt up.This is supposed to be a very long text somewhere that you should not bother to read anyhow. well that doesn't really matter because I will writ it anyway...so please sutt up.This is supposed to be a very long text somewhere that you should not bother to read anyhow. well that doesn't really matter because I will writ it anyway...so please sutt up.This is supposed to be a very long text somewhere that you should not bother to read anyhow. well that doesn't really matter because I will writ it anyway...so please sutt up.This is supposed to be a very long text somewhere that you should not bother to read anyhow. well that doesn't really matter because I will writ it anyway...so please sutt up.
+				</p>
+				
 			</div>
 			<div id="footer">
 				<!-- -->
