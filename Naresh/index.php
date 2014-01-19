05ce5543c7f157ac0c1217084f934bf8e13ed65d
naresh11067 <naresh11067@iiitd.ac.in>
2014-01-19 16:28:27 +0530

    Done little practice of css and added jpg files of logos

--- Naresh/index.php
+++ Naresh/index.php
@@ -1,9 +1,9 @@
 <?php /* Sample page */ ?>
 <html>
 	<head>
-		<title>Welcome here</title>
+		<title>Madhavs Page</title>
 		<meta charset="UTF-8"/>
-		<meta name="author" content="Shivam Chaturvedi -- The Smarter Techie"/>
+		<meta name="author" content="Naresh Aditya Madhav -- The Smarter Techie"/>
 		<meta name="keywords" content="design for the new blogging platform"/>
 		<script type="text/javascript" src="js/jquery.js"></script>
 		<script type="text/javascript" src="js/main.js"></script>
@@ -13,13 +13,41 @@
 		<div id="container">
 			<div id="header">
 				<!-- Head section. Can contain the toolbar or the nav bar if needed or can be merged with the main content area-->
+				<div id="navbar">
+					<ul id="menu">
+						<li class="item">
+							<div>Item 1</div>
+							<ul class="submenu">
+								<li class="submenu-item">Submenu1 item1</li>
+								<li class="submenu-item">Submenu1 item2</li>
+							</ul>
+						</li>
+						<li class="item">
+							<div>Item 2</div>
+							<ul class="submenu">
+								<li class="submenu-item">Submenu2 item1</li>
+								<li class="submenu-item">Submenu2 item2</li>
+								<li class="submenu-item">Submenu2 item3</li>
+							</ul>
+						</li>
+						<li class="item">
+							<div>Item 3</div>
+							<ul class="submenu">
+								<li class="submenu-item">Submenu3 item1</li>
+								<li class="submenu-item">Submenu3 item2</li>
+								<li class="submenu-item">Submenu3 item3</li>
+							</ul>
+						</li>
+					</ul>
+				</div>
 			</div>
 			<div id="wrapper">
+				<img id="headerimage" src="images/HeaderLogo-0.jpg" style="height: 100px;width: 100px;"/>
 				<!-- 	wrapper will contain the main article section plus the side bar.
 						So, this in effect wraps the content box and the sidebar. -->
 			</div>
 			<div id="footer">
-				<!-- -->
+				<!-- Will be having the copyright stuff and some links like about us.  -->
 			</div>
 		</div>
 	</body>
