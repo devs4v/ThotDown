76387937c38d07f04835b10334d59ee4d08714bb
SrishtiSengupta <srishti.sengupta@gmail.com>
2014-01-23 02:27:15 +0530

    All the editing we did today with the main.css files and index files

--- Srishti/index.php
+++ Srishti/index.php
@@ -17,6 +17,52 @@
 			<div id="wrapper">
 				<!-- 	wrapper will contain the main article section plus the side bar.
 						So, this in effect wraps the content box and the sidebar. -->
+				<div id="menuwrap">
+					<ul id="menu">
+						<li class="item yellowtext">Item1</li>
+						<li class="item">Item2</li>
+						<li class="item">Item3</li>
+					</ul>
+				</div>
+				<br/>
+				<br/>
+				<br/>
+				<br/>
+				<br/>
+				<h1>
+					THOTDOWN :D :D :D :D
+				</h1>
+				<img src = "images/pen_01_446.jpg"/>
+				<h2>
+					This is smaller :D :P
+				</h2>
+				<h3>
+					This is even smaller :P
+				</h3>
+				<h4>
+					This is even even more smaller :P
+				</h4>
+				<h5>
+					Ok..lets see...I'm not sure about this one..ok..this is the point where Python is bouncing a ping pong ball around :P
+				</h5>
+				<h6>
+					This is the smallest :P...this is tiiiiiiinnnnnny.. :D
+				</h6>
+				<img src="images/book.jpg" style="float: left;height: 100px;width: 100px;"/>
+				<img src="images/book.jpg" style="float: left;height: 100px;width: 100px;"/>
+				<img src="images/book.jpg" style="float: left;height: 100px;width: 100px;"/>
+				<img src="images/book.jpg" style="float: left;height: 100px;width: 100px;"/>
+				<img src="images/book.jpg" style="float: left;height: 100px;width: 100px;"/>
+				<img src="images/book.jpg" style="float: left;height: 100px;width: 100px;"/>
+				<br/>
+				<br/>
+				<br/>
+				<br/>
+				
+				<p>
+					JE BAAT :D :D :D :D :D :D Ye toh sahi baat hai ki main kuch khas nahi likh raha lekin padh lo apna kya hai time toh khoob sara nahi hai lekin main bore karne mein expert hun toh koi dikkat nahi hai you see.. toh waise toh kya hai ki yeh sab aise hi likha ja raha hun main toh bhai ekdum mast ekdum. toh ye sab itna bada likhne ka yahi maksad tha ki aeinvain bohot sara likhna tha.Ye toh sahi baat hai ki main kuch khas nahi likh raha lekin padh lo apna kya hai time toh khoob sara nahi hai lekin main bore karne mein expert hun toh koi dikkat nahi hai you see.. toh waise toh kya hai ki yeh sab aise hi likha ja raha hun main toh bhai ekdum mast ekdum. toh ye sab itna bada likhne ka yahi maksad tha ki aeinvain bohot sara likhna tha.Ye toh sahi baat hai ki main kuch khas nahi likh raha lekin padh lo apna kya hai time toh khoob sara nahi hai lekin main bore karne mein expert hun toh koi dikkat nahi hai you see.. toh waise toh kya hai ki yeh sab aise hi likha ja raha hun main toh bhai ekdum mast ekdum. toh ye sab itna bada likhne ka yahi maksad tha ki aeinvain bohot sara likhna tha.Ye toh sahi baat hai ki main kuch khas nahi likh raha lekin padh lo apna kya hai time toh khoob sara nahi hai lekin main bore karne mein expert hun toh koi dikkat nahi hai you see.. toh waise toh kya hai ki yeh sab aise hi likha ja raha hun main toh bhai ekdum mast ekdum. toh ye sab itna bada likhne ka yahi maksad tha ki aeinvain bohot sara likhna tha.Ye toh sahi baat hai ki main kuch khas nahi likh raha lekin padh lo apna kya hai time toh khoob sara nahi hai lekin main bore karne mein expert hun toh koi dikkat nahi hai you see.. toh waise toh kya hai ki yeh sab aise hi likha ja raha hun main toh bhai ekdum mast ekdum. toh ye sab itna bada likhne ka yahi maksad tha ki aeinvain bohot sara likhna tha.Ye toh sahi baat hai ki main kuch khas nahi likh raha lekin padh lo apna kya hai time toh khoob sara nahi hai lekin main bore karne mein expert hun toh koi dikkat nahi hai you see.. toh waise toh kya hai ki yeh sab aise hi likha ja raha hun main toh bhai ekdum mast ekdum. toh ye sab itna bada likhne ka yahi maksad tha ki aeinvain bohot sara likhna tha.Ye toh sahi baat hai ki main kuch khas nahi likh raha lekin padh lo apna kya hai time toh khoob sara nahi hai lekin main bore karne mein expert hun toh koi dikkat nahi hai you see.. toh waise toh kya hai ki yeh sab aise hi likha ja raha hun main toh bhai ekdum mast ekdum. toh ye sab itna bada likhne ka yahi maksad tha ki aeinvain bohot sara likhna tha.Ye toh sahi baat hai ki main kuch khas nahi likh raha lekin padh lo apna kya hai time toh khoob sara nahi hai lekin main bore karne mein expert hun toh koi dikkat nahi hai you see.. toh waise toh kya hai ki yeh sab aise hi likha ja raha hun main toh bhai ekdum mast ekdum. toh ye sab itna bada likhne ka yahi maksad tha ki aeinvain bohot sara likhna tha.
+				</p>
+				
 			</div>
 			<div id="footer">
 				<!-- -->
