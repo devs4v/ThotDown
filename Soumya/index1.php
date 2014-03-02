69bb883f3f21d05f6fbf5e1f9b8372b47e572537
Soumya-13106 <soumya13106@iiitd.ac.in>
2014-03-02 21:22:51 +0530

    Sign-Up Form
    To be customised according to final decision.

---
+++ Soumya/index1.php
@@ -0,0 +1,44 @@
+<?php /* Sample page */ ?>
+<html>
+	<head>
+		<title>Welcome here</title>
+		<meta charset="UTF-8"/>
+		<meta name="author" content="Shivam Chaturvedi -- The Smarter Techie"/>
+		<meta name="keywords" content="design for the new blogging platform"/>
+		<script type="text/javascript" src="js/jquery.js"></script>
+		<script type="text/javascript" src="js/main.js"></script>
+		<link rel="stylesheet" href="css/main1.css"/>
+	</head>
+	<body>
+		<div id="container">
+			<div id="header">
+				<!-- Head section. Can contain the toolbar or the nav bar if needed or can be merged with the main content area-->
+			</div>
+			<div id="wrapper">
+				<!-- 	wrapper will contain the main article section plus the side bar.
+						So, this in effect wraps the content box and the sidebar. -->
+			<div id="sign-up">
+				<form>
+					<div class="signup-index">  First name: </div>
+					<div class="signup-field"> <input type="text" value="first name" name="firstname"></div><br>
+					<div class="signup-index">  Last name: </div>
+					<div class="signup-field"> <input type="text" value="last name" name="lastname"></div><br>
+					<div class="signup-index">  E-mail ID: </div>
+					<div class="signup-field"> <input type="text" value="email id" name="email id"></div><br>
+					<div class="signup-index">  User Name: </div>
+					<div class="signup-field"> <input type="text" value="user name" name="username"></div><br>
+					<div class="signup-index">  Password: </div>
+					<div class="signup-field"> <input type="password" value="pass" name="password"></div><br>
+					<div class="signup-index">  Confirm Password: </div>
+					<div class="signup-field"> <input type="password" value="pass" name="confirm password"></div><br>
+					<div class="signup-index"><input type="submit" value="Submit"></div>
+					<div class="signup-index"><input type="reset" value="Reset"></div>
+
+				</form>
+			</div>
+			<div id="footer">
+				<!-- -->
+			</div>
+		</div>
+	</body>
+</html>
