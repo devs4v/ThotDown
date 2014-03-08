be570d12c3d32402cfac16d73c526bd94d0342a1
Anjali <ujjainia.2906@gmail.com>
2014-03-09 05:14:34 +0530

    sample page3 done
    some more things are need to be added
    but just assure me if this is good enough?

---
+++ Anjali/sample_page3.php
@@ -0,0 +1,31 @@
+<?php /* Sample page */ ?>
+<!-- 1 page with images and all, like a photo blog and how the layout should behave to such a blog. -->
+<html>
+	<head>
+		<title>Welcome here</title>
+		<meta charset="UTF-8"/>
+		<meta name="author" content="Shivam Chaturvedi -- The Smarter Techie"/>
+		<meta name="keywords" content="design for the new blogging platform"/>
+		<script type="text/javascript" src="js/jquery.js"></script>
+		<script type="text/javascript" src="js/main.js"></script>
+		<script type="type/javascript" src="js/jquery.scrollTo-1.4.3.1-min.js"></script>
+		<link href='http://fonts.googleapis.com/css?family=Averia+Sans+Libre' rel='stylesheet' type='text/css'>
+		<link href='http://fonts.googleapis.com/css?family=Sigmar+One' rel='stylesheet' type='text/css'>
+		<link rel="stylesheet" href="css/sample3.css"/>
+
+	</head>
+	<body>
+		<div class="container">
+			<div id="heading">
+				<h1>FRIENDS</h1>
+				<div class="describe">Walking with a friend in the dark is better than walking alone in the light.</div>
+			</div>
+			<div class="size"><img src="pics/frnds2.jpg"/>
+			</div>
+			<div class="size"><img src="pics/frnds1.jpg"/>
+			</div>
+			<div class="size"><img src="pics/frnds3.jpg"/>
+			</div>
+		</div>
+	</body>
+</html>
