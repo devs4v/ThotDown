05cf0d1c7565a711e73316ba8e607af345e96485
Soumya-13106 <soumya13106@iiitd.ac.in>
2014-03-12 01:12:59 +0530

    Page with Videos
    I know its not so good. And the color-scheme sucks! But please suggest
    how to improve.

---
+++ Soumya/publicblog.php
@@ -0,0 +1,81 @@
+<?php /* Sample page */ ?>
+<html>
+	<head>
+		<title>Welcome here</title>
+		<meta charset="UTF-8"/>
+		<meta name="author" content="Shivam Chaturvedi -- The Smarter Techie"/>
+		<meta name="keywords" content="design for the new blogging platform"/>
+		<script type="text/javascript" src="js/jquery.js"></script>
+		<script type="text/javascript" src="js/main.js"></script>
+		<link rel="stylesheet" href="css/publicblog.css"/>
+	</head>
+	<body>
+		<div id="container">
+			<div id="header">
+				<!-- Head section. Can contain the toolbar or the nav bar if needed or can be merged with the main content area-->
+				<div id="main_logo">
+					<img src="pics/thotDownLogo.png" id="logo_header"/>
+				</div>
+				<div id="HeaderContent_area">
+					<ul id="menu">
+						<li class="item"><span>Sign In/Up</span></li>
+						<li class="item"><span>About Us</span></li>
+						<li class="item"><span>Events</span></li>
+						<li class="item"><span>Contact Us</span></li>
+					</ul>
+				</div>
+			</div>
+			<div id="wrapper">
+				<div id="content">
+					<div class="content-heading">Qtiyappa</div>
+					<h4 class="content-writeup">TVF : India's 1st Online Youth Entertainment Network<br>
+					TheViralFever.Com is India's 1st Organized OnlineTV, where you can watch Regular Shows created especially for the young audience.<br>
+					SUBSCRIBE for your weekly dose of Humor & Q-Tiyapa.</h4>
+					<div class="videos-border">
+						<div class="videos">
+							<iframe width="640" height="360" src="//www.youtube.com/embed/qR9y6hkXjDs?feature=player_detailpage" frameborder="0" allowfullscreen></iframe>
+						</div>
+					</div>
+					<p><p><p>
+					<div class="videos-border">
+						<div class="videos">
+							<iframe width="640" height="360" src="//www.youtube.com/embed/ZYDGmkjK9pI?feature=player_detailpage" frameborder="0" allowfullscreen></iframe>
+						</div>
+					</div>
+					<p><p><p>
+					<h4 class="content-writeup">Published on Jun 18, 2013<br>
+					Download Lorde's album "Pure Heroine (Extended Edition)" featuring "Royals" now:<br>
+					iTunes: <a href="http://smarturl.it/PureHeroineExtended">http://smarturl.it/PureHeroineExtended</a><br>
+					Amazon: <a href="http://smarturl.it/PureHeroineExtendAZ">http://smarturl.it/PureHeroineExtendAZ</a><br>
+					<p>
+					Music video by Lorde performing Royals. (C) 2013 Universal Music NZ Ltd.<br>
+					You can also watch the video at: <a href="http://www.youtube.com/watch?feature=player_detailpage&v=nlcIKh6sBtc">http://www.youtube.com/watch?feature=player_detailpage&v=nlcIKh6sBtc</a><br></h4>
+					<div class="videos-border">
+						<div class="videos">
+							<iframe width="640" height="360" src="//www.youtube.com/embed/nlcIKh6sBtc?feature=player_detailpage" frameborder="0" allowfullscreen></iframe>
+						</div>
+					</div>
+					<p><p><p>
+				</div>
+			</div>
+			<div id="footer">
+				<div class="container">
+					<div class="navigation">
+				
+						<a class = "buttons" href = "#">Help</a>
+						
+						<a class = "buttons" href = # >Terms</a>
+						
+						<a class = "buttons" href = # >About Us</a>
+						
+						<a class = "buttons" href = # >Licence</a>
+						
+						<a class = "buttons" href = # >Contact</a>
+						
+					</div>
+					<div id = "copyright_line"> Copyright &copy; 2014 The Smarter Techies </div>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
