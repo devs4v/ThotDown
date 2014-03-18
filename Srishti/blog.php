0e8477392feb0a215e57689b060e95738f6208c3
SrishtiSengupta <srishti.sengupta@gmail.com>
2014-03-18 17:15:04 +0530

    checking out Sumit's design

---
+++ Srishti/blog.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+	<!-- stylesheet-->
+	<link rel="stylesheet" href="blog.css" type="text/css">
+
+</head>
+
+<body>
+
+	<div id="title_container">
+
+		<div id="title_box">
+			<h1><u>This is a heading</u></h1>
+		</div>
+
+		<div id="subtitle_box">
+			
+
+			<div id="sub_title">
+				
+			</div>
+
+			<div id="date_right">
+				<p><u>Blog posted on:</u></br>10/05/2013</p>
+			</div>
+
+		</div>
+
+	</div>
+
+	<div id="main_box">
+		
+		<div id="blog">
+			<p>He difficult contented we determine ourselves me am earnestly. Hour no find it park. Eat welcomed any husbands moderate. Led was misery played waited almost cousin living. Of intention contained is by middleton am. Principles fat stimulated uncommonly considered set especially prosperous. Sons at park mr meet as fact like. </br></br>
+
+			Must you with him from him her were more. In eldest be it result should remark vanity square. Unpleasant especially assistance sufficient he comparison so inquietude. Branch one shy edward stairs turned has law wonder horses. Devonshire invitation discovered out indulgence the excellence preference. Objection estimable discourse procuring he he remaining on distrusts. Simplicity affronting inquietude for now sympathize age. She meant new their sex could defer child. An lose at quit to life do dull. 
+
+			At distant inhabit amongst by. Appetite welcomed interest the goodness boy not. Estimable education for disposing pronounce her. John size good gay plan sent old roof own. Inquietude saw understood his friendship frequently yet. Nature his marked ham wished. 
+
+			Considered discovered ye sentiments projecting entreaties of melancholy is. In expression an solicitude principles in do. Hard do me sigh with west same lady. Their saved linen downs tears son add music. Expression alteration entreaties mrs can terminated estimating. Her too add narrow having wished. To things so denied admire. Am wound worth water he linen at vexed. </br></br>
+
+			So if on advanced addition absolute received replying throwing he. Delighted consisted newspaper of unfeeling as neglected so. Tell size come hard mrs and four fond are. Of in commanded earnestly resources it. At quitting in strictly up wandered of relation answered felicity. Side need at in what dear ever upon if. Same down want joy neat ask pain help she. Alone three stuff use law walls fat asked. Near do that he help. 
+
+			He went such dare good mr fact. The small own seven saved man age ﻿no offer. Suspicion did mrs nor furniture smallness. Scale whole downs often leave not eat. An expression reasonably cultivated indulgence mr he surrounded instrument. Gentleman eat and consisted are pronounce distrusts. </p>
+
+			
+
+		</div>
+
+		<div id="links">
+			
+		</div>
+
+	</div>
+
+	
+
+</body>
+
+</html>
