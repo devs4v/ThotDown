4fc748771c8bd31740453b28652dafa226db362c
naresh11067 <naresh11067@iiitd.ac.in>
2014-03-19 02:11:08 +0530

    Made changes in Soumya's folder (publicblog.php)
    Please check for further improvement as only a general layout design is
    made.

--- Soumya/publicblog.php
+++ Soumya/publicblog.php
@@ -1,9 +1,9 @@
 <?php /* Sample page */ ?>
 <html>
 	<head>
-		<title>Welcome here</title>
+		<title>Qtiyappa</title>
 		<meta charset="UTF-8"/>
-		<meta name="author" content="Shivam Chaturvedi -- The Smarter Techie"/>
+		<meta name="author" content="Soumya Sharma - The Smarter Techie"/>
 		<meta name="keywords" content="design for the new blogging platform"/>
 		<script type="text/javascript" src="js/jquery.js"></script>
 		<script type="text/javascript" src="js/main.js"></script>
@@ -14,42 +14,61 @@
 			<div id="header">
 				<!-- Head section. Can contain the toolbar or the nav bar if needed or can be merged with the main content area-->
 				<div id="main_logo">
-					<img src="pics/thotDownLogo.png" id="logo_header"/>
+					<img src="pics/thotDownLogo.png" id="logo_header" style="height:25px; width:20px;"/>
 				</div>
 				<div id="HeaderContent_area">
 					<ul id="menu">
-						<li class="item"><span>Sign In/Up</span></li>
-						<li class="item"><span>About Us</span></li>
-						<li class="item"><span>Events</span></li>
-						<li class="item"><span>Contact Us</span></li>
+						<li class="item"><span class="itemh"><a  href="#" style="text-decoration: none;color:#FFF;" onMouseOver="this.style.color='yellow'"
+   onMouseOut="this.style.color='#FFF'">DashBoard</a></span></li>
+						<li class="item"><span class="itemh"><a  href="#" style="text-decoration: none;color:#FFF;" onMouseOver="this.style.color='yellow'"
+   onMouseOut="this.style.color='#FFF'">About Us</a></span></li>
+						<li class="item"><span class="itemh"><a  href="#" style="text-decoration: none;color:#FFF;" onMouseOver="this.style.color='yellow'"
+   onMouseOut="this.style.color='#FFF'">Events</a></span></li>
+						<li class="item"><span class="itemh"><a  href="#" style="text-decoration: none;color:#FFF;" onMouseOver="this.style.color='yellow'"
+   onMouseOut="this.style.color='#FFF'">signout</a></span></li>
 					</ul>
 				</div>
 			</div>
 			<div id="wrapper">
 				<div id="content">
-					<div class="content-heading">Qtiyappa</div>
-					<h4 class="content-writeup">TVF : India's 1st Online Youth Entertainment Network<br>
-					TheViralFever.Com is India's 1st Organized OnlineTV, where you can watch Regular Shows created especially for the young audience.<br>
-					SUBSCRIBE for your weekly dose of Humor & Q-Tiyapa.</h4>
-					<div class="videos">
-							<iframe width="640" height="360" src="//www.youtube.com/embed/qR9y6hkXjDs?feature=player_detailpage" frameborder="0" allowfullscreen></iframe>
+					<div id="nonsidebar"> 
+						<div class="content-heading">Qtiyappa</div>
+						<h4 class="content-writeup">TVF : India's 1st Online Youth Entertainment Network<br>
+						TheViralFever.Com is India's 1st Organized OnlineTV, where you can watch Regular Shows created especially for the young audience.<br>
+						SUBSCRIBE for your weekly dose of Humor & Q-Tiyapa.</h4>
+						<div class="videos-border">
+							<div class="videos">
+								<iframe width="500" height="250" src="//www.youtube.com/embed/qR9y6hkXjDs?feature=player_detailpage" frameborder="0" allowfullscreen></iframe>
+							</div>
+						</div>
+						</br></br></br>
+						<div class="videos-border">
+							<div class="videos">
+								<iframe width="500" height="250" src="//www.youtube.com/embed/ZYDGmkjK9pI?feature=player_detailpage" frameborder="0" allowfullscreen></iframe>
+							</div>
+						</div>
+						</br></br></br>
+						<h4 class="content-writeup">Published on Jun 18, 2013<br>
+						Download Lorde's album "Pure Heroine (Extended Edition)" featuring "Royals" now:<br>
+						iTunes: <a href="http://smarturl.it/PureHeroineExtended">http://smarturl.it/PureHeroineExtended</a><br>
+						Amazon: <a href="http://smarturl.it/PureHeroineExtendAZ">http://smarturl.it/PureHeroineExtendAZ</a><br>
+						</br>
+						Music video by Lorde performing Royals. (C) 2013 Universal Music NZ Ltd.<br>
+						You can also watch the video at: <a href="http://www.youtube.com/watch?feature=player_detailpage&v=nlcIKh6sBtc">http://www.youtube.com/watch?feature=player_detailpage&v=nlcIKh6sBtc</a><br></h4>
+						<div class="videos-border">
+							<div class="videos">
+								<iframe width="500" height="250" src="//www.youtube.com/embed/nlcIKh6sBtc?feature=player_detailpage" frameborder="0" allowfullscreen></iframe>
+							</div>
+						</div>
+						</br></br></br>
 					</div>
-					<br><br><br>
-					<div class="videos">
-							<iframe width="640" height="360" src="//www.youtube.com/embed/ZYDGmkjK9pI?feature=player_detailpage" frameborder="0" allowfullscreen></iframe>
+					<div id="sidebar">
+						</br></br>
+						<h2> Archive</h2>
+						</br></br></br></br></br></br></br>
+						<h2> Contributors </h2>
+						</br></br></br></br></br></br></br>
 					</div>
-					<br><br><br>
-					<h4 class="content-writeup">Published on Jun 18, 2013<br>
-					Download Lorde's album "Pure Heroine (Extended Edition)" featuring "Royals" now:<br>
-					iTunes: <a href="http://smarturl.it/PureHeroineExtended">http://smarturl.it/PureHeroineExtended</a><br>
-					Amazon: <a href="http://smarturl.it/PureHeroineExtendAZ">http://smarturl.it/PureHeroineExtendAZ</a><br>
-					<p>
-					Music video by Lorde performing Royals. (C) 2013 Universal Music NZ Ltd.<br>
-					You can also watch the video at: <a href="http://www.youtube.com/watch?feature=player_detailpage&v=nlcIKh6sBtc">http://www.youtube.com/watch?feature=player_detailpage&v=nlcIKh6sBtc</a><br></h4>
-					<div class="videos">
-							<iframe width="640" height="360" src="//www.youtube.com/embed/nlcIKh6sBtc?feature=player_detailpage" frameborder="0" allowfullscreen></iframe>
-					</div>
-					<br><br><br>
 				</div>
 			</div>
 			<div id="footer">
