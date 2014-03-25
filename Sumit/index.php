bf7bc09101599b1357482d6f1504358c5ebeb7e2
Sumit Keswani <sumitkeswani123@gmail.com>
2014-03-26 01:33:14 +0530

    Basic structure of blog

--- Sumit/index.php
+++ Sumit/index.php
@@ -17,84 +17,51 @@
         <meta name="author" content="Codrops" />
         <link rel="shortcut icon" href="../favicon.ico"> 
         <link rel="stylesheet" type="text/css" href="css/demo.css" />
-        <link rel="stylesheet" type="text/css" href="css/style.css" />
+        <link rel="stylesheet" type="text/css" href="css/main.css" />
 		<script type="text/javascript" src="js/modernizr.custom.28468.js"></script>
 		<link href='http://fonts.googleapis.com/css?family=Economica:700,400italic' rel='stylesheet' type='text/css'>
 		<noscript>
 			<link rel="stylesheet" type="text/css" href="css/nojs.css" />
 		</noscript>
 	</head>
+
 	<body>
-		<div id="container">
-			<div id="header">
-				<!-- Head section. Can contain the toolbar or the nav bar if needed or can be merged with the main content areas-->
+
+		<div id="title_container">
+
+			<div id="title_box">
+				<h1><u>This is a heading</u></h1>
 			</div>
-					<div id="da-slider" class="da-slider">
-						<div class="da-slide">
-							<h2>THOT-DOWN</h2>
-							<h1>whatever comes to your mind , just Jot Down</h1>
-							<p>This is a blogging platform where you can work more interactively.</p>
-							<a href="#" class="da-link">More Info</a>
-							<div class="da-img"><img src="1.jpg" style="height:300px; width:400px;" alt="images/1.png"/></div>
-						</div>
-						<div class="da-slide">
-							<h2>Easy management</h2>
-							<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
-							<a href="#" class="da-link">Read more</a>
-							<div class="da-img"><img src="images/2.png" alt="image01" /></div>
-						</div>
-						<div class="da-slide">
-							<h2>Revolution</h2>
-							<p>A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.</p>
-							<a href="#" class="da-link">Read more</a>
-							<div class="da-img"><img src="images/3.png" alt="image01" /></div>
-						</div>
-						<div class="da-slide">
-							<h2>Quality Control</h2>
-							<p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar.</p>
-							<a href="#" class="da-link">Read more</a>
-							<div class="da-img"><img src="images/4.png" alt="image01" /></div>
-						</div>
-						<nav class="da-arrows">
-							<span class="da-arrows-prev"></span>
-							<span class="da-arrows-next"></span>
-						</nav>
-					</div>
-			<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
-			<script type="text/javascript" src="js/jquery.cslider.js"></script>
-			<script type="text/javascript">
-				$(function() {
-			
-				$('#da-slider').cslider({
-					autoplay	: true,
-					bgincrement	: 450
-				});
-			
-			});
-			</script>
-			<div id="wrapper">
-				<!-- 	wrapper will contain the main article section plus the side bar.
-						So, this in effect wraps the content box and the sidebar. -->
-			</div>
-			<div id="footer">
-				<div class="footer-container">
-					<div class="navigation">
-				
-						<a class = "buttons" href = "#">Help</a>
-						
-						<a class = "buttons" href = "#" >Terms</a>
-						
-						<a class = "buttons" href = "#" >About Us</a>
-						
-						<a class = "buttons" href = "#" >Licence</a>
-						
-						<a class = "buttons" href = "#" >Contact</a>
-						
-					</div>
-					<div id = "copyright_line"> Copyright &copy; 2014 The Smarter Techies </div>
+
+			<div id="subtitle_box">	
+
+				<div id="sub_title">		
 				</div>
+
+				<div id="date_right">
+					<p><u>Blog posted on:</u></br>10/05/2013</p>
+				</div>
+
 			</div>
+
 		</div>
+
+		<div id="main_box">
+		
+			<div id="blog">
+				<p>He difficult contented we determine ourselves me am earnestly. Hour no find it park. Eat welcomed any husbands moderate. Led was misery played waited almost cousin living. Of intention contained is by middleton am. Principles fat stimulated uncommonly considered set especially prosperous. Sons at park mr meet as fact like. </br></br>
+				Must you with him from him her were more. In eldest be it result should remark vanity square. Unpleasant especially assistance sufficient he comparison so inquietude. Branch one shy edward stairs turned has law wonder horses. Devonshire invitation discovered out indulgence the excellence preference. Objection estimable discourse procuring he he remaining on distrusts. Simplicity affronting inquietude for now sympathize age. She meant new their sex could defer child. An lose at quit to life do dull. 
+				At distant inhabit amongst by. Appetite welcomed interest the goodness boy not. Estimable education for disposing pronounce her. John size good gay plan sent old roof own. Inquietude saw understood his friendship frequently yet. Nature his marked ham wished. 
+				Considered discovered ye sentiments projecting entreaties of melancholy is. In expression an solicitude principles in do. Hard do me sigh with west same lady. Their saved linen downs tears son add music. Expression alteration entreaties mrs can terminated estimating. Her too add narrow having wished. To things so denied admire. Am wound worth water he linen at vexed. </br></br>
+				So if on advanced addition absolute received replying throwing he. Delighted consisted newspaper of unfeeling as neglected so. Tell size come hard mrs and four fond are. Of in commanded earnestly resources it. At quitting in strictly up wandered of relation answered felicity. Side need at in what dear ever upon if. Same down want joy neat ask pain help she. Alone three stuff use law walls fat asked. Near do that he help. 
+				He went such dare good mr fact. The small own seven saved man age ﻿no offer. Suspicion did mrs nor furniture smallness. Scale whole downs often leave not eat. An expression reasonably cultivated indulgence mr he surrounded instrument. Gentleman eat and consisted are pronounce distrusts. </p>
+			</div>
+
+			<div id="links">			
+			</div>
+
+		</div>
+
 	</body>
 </html>
 
