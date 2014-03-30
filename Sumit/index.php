7f024f1218f19c9e630f28817c07bae505905de7
Sumit Keswani <sumitkeswani123@gmail.com>
2014-03-30 19:40:13 +0530

    revised structure of blog

--- Sumit/index.php
+++ Sumit/index.php
@@ -26,43 +26,57 @@
 	</head>
 
 	<body>
-
-		<div id="title_container">
-
-			<div id="title_box">
-				<h1><u>This is a heading</u></h1>
+		<div class="Blog-container">
+			
+			<div class="Title-box">
+				<h1 class="Blog-title"><u>This is Heading</u></h1>
 			</div>
 
-			<div id="subtitle_box">	
-
-				<div id="sub_title">		
+			<div class="Subtitle-box">
+				<div class="subtitle">
+					<h3 class="pink">This is the subtitle + random text + more random text</h3>
 				</div>
 
-				<div id="date_right">
-					<p><u>Blog posted on:</u></br>10/05/2013</p>
+				<div class="date-box">
+					<p>Sunday, January 15, 2013</p>
 				</div>
-
 			</div>
 
-		</div>
+			<div class="Blog">
+				<p class="blog-text">How good is the education being provided by an academic Institution? While there can be sophesticated ways of assessing the quality (e.g. by having peope who understand education and its goals assess the quality by looking at the faculty, courses, method of teaching, etc.) often quality of the overall education is assessed by how well their graduates do in their careers. And an indicator of this is how good a start the graduates get, i.e. the placement record of Institute.</br></br>
 
-		<div id="main_box">
-		
-			<div id="blog">
-				<p>He difficult contented we determine ourselves me am earnestly. Hour no find it park. Eat welcomed any husbands moderate. Led was misery played waited almost cousin living. Of intention contained is by middleton am. Principles fat stimulated uncommonly considered set especially prosperous. Sons at park mr meet as fact like. </br></br>
-				Must you with him from him her were more. In eldest be it result should remark vanity square. Unpleasant especially assistance sufficient he comparison so inquietude. Branch one shy edward stairs turned has law wonder horses. Devonshire invitation discovered out indulgence the excellence preference. Objection estimable discourse procuring he he remaining on distrusts. Simplicity affronting inquietude for now sympathize age. She meant new their sex could defer child. An lose at quit to life do dull. 
-				At distant inhabit amongst by. Appetite welcomed interest the goodness boy not. Estimable education for disposing pronounce her. John size good gay plan sent old roof own. Inquietude saw understood his friendship frequently yet. Nature his marked ham wished. 
-				Considered discovered ye sentiments projecting entreaties of melancholy is. In expression an solicitude principles in do. Hard do me sigh with west same lady. Their saved linen downs tears son add music. Expression alteration entreaties mrs can terminated estimating. Her too add narrow having wished. To things so denied admire. Am wound worth water he linen at vexed. </br></br>
-				So if on advanced addition absolute received replying throwing he. Delighted consisted newspaper of unfeeling as neglected so. Tell size come hard mrs and four fond are. Of in commanded earnestly resources it. At quitting in strictly up wandered of relation answered felicity. Side need at in what dear ever upon if. Same down want joy neat ask pain help she. Alone three stuff use law walls fat asked. Near do that he help. 
-				He went such dare good mr fact. The small own seven saved man age ﻿no offer. Suspicion did mrs nor furniture smallness. Scale whole downs often leave not eat. An expression reasonably cultivated indulgence mr he surrounded instrument. Gentleman eat and consisted are pronounce distrusts. </p>
+				Most good institutions have good placement records. While good institutions have good placement/careers statistics, there has been a perpetual debate/doubt whether this outcome is due to the fact that these institutes, being higly selective, take in the brightest of the students (if you take the top 5% of the students, of course they will do well – the argument goes), or whether it is due to the value of education they provide.  All of us academics in top institutions believe that while high quality of intake has a role to play, the quality of education provided at these institutions is the key determining factor in the success of these students. Unfortunately, this point cannot be supported by data as it is not possible get the desired data (e.g. average students taken in the top institutions and provided good education and then seeing how they do.)</br></br>
+
+				At IIIT Delhi, as an Institute which has now evolved into a sought-after, high quality institution, we have a limited data from the first batch to shed some light to this debate. In 2008 the Institute was started – I was appointed the Director (employee #001) in Aug, and we had to start the new session in Sept. As all the admissions were over, the “best” students were already gone – even if they were not, why will they join an Institute that just started and had no faculty or facilities. In any case, we did an entrance test with an eligibility criteria being 60% in class XII.  About 350 students applied, finally around 250 or so appeared, of which we selected 60 – and many of those who were offered did not accept it. Effectively, almost one in every three students who wanted was admitted.   (Now that we have established ourselves as a top Institution,  of those who apply – and only those above 80% in class XII can apply – about 6% are selected.)</br></br>
+
+				So, clearly the first batch was hardly selective by Indian standards. More so, given the fact that admissions in almost all major Institutions were over when we started our process (our exam was held in Aug!).</br></br>
+
+				After admisison, this batch was taught in their first by guest faculty in borrowed facilites, but using our program and curriculum. In second year, many of the courses were taught by our faculty. It is only by third year when almost all the Computing courses were being taught by our faculty. In other words, whatever is our quality of education now, the first batch did not get it fully.</br></br>
+
+				Fast forward to 2012, when this batch graduated. The Institute had no track record and no placement history – making it extremely hard to convince companies to visit for campus placement, particularly since we wanted to focus on technology companies as we felt that our students were far too well trained for the “regular” software jobs (by this I mean the entry level jobs offered by the large software houses in the country and where perhaps 90% of the CS graduates in the country get their employment. These jobs, it is well know, have a starting package of about Rs 3 to 3.5 Lac). With effort, we were able to convince some of the companies to come.</br></br>
+
+				Almost every company that visited, despite their initial doubts, recruited some students. Here we are talking about tech companies that generally have a rigorous selection process – test, multiple rounds of intereviews, etc. Some companies were so happy by the quality of graduating students that they recruited far more than what their best case scenario was (as we were told.) One company, which came for recruiting for their research lab, then called in their other product divisions to recurit. Some highly selective companies which visit few campuses in the country told us that they felt that the graduates are as good as other places they visit.</br></br>
+
+				The final outcome in numbers: The average salary offer is about Rs 7 Lac (twice that of a regular software job), with majority getting offers of more than Rs 6 Lac, and about 10%  getting offers of more than Rs 10 Lac.</br></br>
 			</div>
 
-			<div id="links">			
+			<div class="author-time-box">
+				<p class="pink"><u><b>Posted By :</b></u></p>
+				<p>YO YO..Honey Singh @ 4:20 pm</p>
+			</div>	
+
+			<div class="MBTFP">
+				<p>M B T F P</p>
 			</div>
 
-		</div>
+			<div class="Labels">
+				<p class="pink"><b><u>Labels :</b></u></p>
+				<p>IIITD, Placements, Education</p>
+			</div>
 
+		</div
 	</body>
+
 </html>
 
 
