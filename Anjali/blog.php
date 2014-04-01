dba030a58514221a6977337473c923e71aea16d9
Anjali <ujjainia.2906@gmail.com>
2014-04-01 20:42:43 +0530

    blog.php
    My page for pics and some text
    modified sumit's page only with some of the font change :P
    and mine work too..:D

---
+++ Anjali/blog.php
@@ -0,0 +1,70 @@
+<html>
+	<head>
+		<title>Blog</title>
+		<meta charset="UTF-8"/>
+		<meta name="author" content="Shivam Chaturvedi -- The Smarter Techie"/>
+		<meta name="keywords" content="design for the new blogging platform"/>
+		<script type="text/javascript" src="js/jquery.js"></script>
+		<script type="text/javascript" src="js/main.js"></script>
+		<script type="type/javascript" src="js/jquery.scrollTo-1.4.3.1-min.js"></script>
+		<link href='http://fonts.googleapis.com/css?family=Averia+Sans+Libre' rel='stylesheet' type='text/css'>
+		<link href='http://fonts.googleapis.com/css?family=Sigmar+One' rel='stylesheet' type='text/css'>
+		<link rel="stylesheet" href="css/blog.css"/>
+
+	</head>
+	<body>
+		<div class="Blog-container">
+			
+			<div class="Title-box">
+				<h1 class="Blog-title">FRIENDS</h1>
+			</div>
+
+			<div class="Subtitle-box">
+				<div class="subtitle"> Walking with a friend in the dark is better than walking alone in the light.
+				</div>
+
+				<div class="date-box">
+					<p>Thursday, April 3, 2014</p>
+				</div>
+			</div>
+
+			<div class="Blog">
+
+				<div class="Blog-text">
+					<img src="pics/frnds2.jpg"/>
+				</div>
+				<div class="describe">
+					“We'll be Friends Forever, won't we, Pooh?' asked Piglet. Even longer,' Pooh answered.”
+				</div>
+				<div class="Blog-text">
+					<img src="pics/frnds1.jpg"/>
+				</div>
+				<div class="describe">
+					“Friendship is the hardest thing in the world to explain. It's not something you learn in school. But if you haven't learned the meaning of friendship, you really haven't learned anything.” </div>
+				<div class="Blog-text">
+					<img src="pics/frnds3.jpg"/>
+				</div>
+				<div class="describe">
+					“Only a true best friend can protect you from your immortal enemies.” 
+				</div>
+				
+			</div>
+
+			<div class="author-time-box">
+				<p class="pink"><u><b>Posted By :</b></u></p>
+				<p>YO YO..Honey Singh @ 4:20 pm</p>
+			</div>	
+
+			<div class="MBTFP">
+				<p>M B T F P</p>
+			</div>
+
+			<div class="Labels">
+				<p class="pink"><b><u>Labels :</b></u></p>
+				<p>IIITD, Placements, Education</p>
+			</div>
+
+		</div
+	</body>
+
+</html>
