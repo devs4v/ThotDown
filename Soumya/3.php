bf3b00a624c4280b98c27bdc14409b3729116f9c
Soumya-13106 <soumya13106@iiitd.ac.in>
2014-04-03 02:14:13 +0530

    Videos Page
    A plug-in for showing more/less is not working. Will work on it more
    later. Basic pattern is ready. Though Colour Scheme thori dark ho gayi
    na?

---
+++ Soumya/3.php
@@ -0,0 +1,179 @@
+<?php /* Sample page */ ?>
+<html lang="en">
+<html>
+	<head>
+		<title>Welcome here</title>
+		<meta charset="UTF-8"/>
+		<meta name="author" content="Shivam Chaturvedi -- The Smarter Techie"/>
+		<meta name="keywords" content="design for the new blogging platform"/>
+		<script type="text/javascript" src="js/jquery.js"></script>
+		<script type="text/javascript" src="js/more-less.js"></script>
+		<script type="type/javascript" src="js/jquery.scrollTo-1.4.3.1-min.js"></script>
+		<link href='http://fonts.googleapis.com/css?family=Averia+Sans+Libre' rel='stylesheet' type='text/css'>
+		<link href='http://fonts.googleapis.com/css?family=Sigmar+One' rel='stylesheet' type='text/css'>
+		<link rel="stylesheet" href="css/3.css"/>
+		<script type="text/javascript">
+			$(function() {
+			var showChar = 120, showtxt = "more", hidetxt = "less";
+			$('.more').each(function() {
+			var content = $(this).text();
+			if (content.length > showChar) {
+			var con = content.substr(0, showChar);
+			var hcon = content.substr(showChar, content.length - showChar);
+			var txt= con +  '<span class="dots">...</span><span class="morecontent"><span>' + hcon + '</span>&nbsp;&nbsp;<a href="" class="moretxt">' + showtxt + '</a></span>';
+			$(this).html(txt);
+			}
+			});
+			$(".moretxt").click(function() {
+			if ($(this).hasClass("sample")) {
+			$(this).removeClass("sample");
+			$(this).text(showtxt);
+			} else {
+			$(this).addClass("sample");
+			$(this).text(hidetxt);
+			}
+			$(this).parent().prev().toggle();
+			$(this).prev().toggle();
+			return false;
+			});
+			});
+		</script>
+		<!--<style type="text/css">
+		body{
+		font-family: Calibri, Arial;
+		margin: 0px;
+		padding: 0px;
+		}
+		.more {
+		width: 400px;
+		background-color: #f0f0f0;
+		margin: 10px;
+		}
+		.morecontent span {
+		display: none;
+		}
+		</style>-->
+		</head>
+
+	<body>
+		<div class="Blog-container">
+			<div class="Title-box">
+				<h1 class="Blog-title"><u>Qtiyapa</u></h1>
+			</div>
+			<div class="Subtitle-box">
+				<div class="subtitle">
+					<h3 class="pink">TVF : India's 1st Online Youth Entertainment Network<br>
+						TheViralFever.Com is India's 1st Organized OnlineTV, where you can watch Regular Shows created especially for the young audience.<br>
+						SUBSCRIBE for your weekly dose of Humor & Q-Tiyapa.</h3>
+				</div>
+				<div class="date-box">
+					<p>Sunday, January 15, 2013</p>
+				</div>
+			</div>
+			<div class="Blog">
+				<div class="blog-text">
+				<iframe class="videos"src="//www.youtube.com/embed/5zE1gBYVaVk" frameborder="0" allowfullscreen></iframe>
+				<div class="more-less">
+				<div class="more-block">
+					<p class="video-description">
+					"SomEdaY, I waNna GrOW oLd wiV
+					SAm1 ii LuRv && SAm1 WoSz LuRvE
+					me :] "<br>
+					Another day<br>
+					Without your smile<br>
+					Another day just passes by<br>
+					But now I know<br>
+					How much it means<br>
+					For you to stay<br>
+					Right here with me<br>
+					<br>
+					The time we spent apart will make our love grow stronger<br>
+					But it hurt so bad I cant take it any longer<br>
+					<br>
+					I wanna grow old with you<br>
+					I wanna die lying in your arms<br>
+					I wanna grow old with you<br>
+					I wanna be looking in your eyes<br>
+					I wanna be there for you<br>
+					Sharing everything you do<br>
+					I wanna grow old with you<br>
+					<br>
+					A thousand miles between us now<br>
+					It causes me to wonder how<br>
+					Our love tonight remains so strong<br>
+					It makes our risk right all along<br>
+					<br>
+					The time we spent apart will make our love grow stronger<br>
+					But it hurt so bad I cant take it any longer<br>
+					<br>
+					I wanna grow old with you<br>
+					I wanna die lying in your arms<br>
+					I wanna grow old with you<br>
+					I wanna be looking in your eyes<br>
+					I wanna be there for you<br>
+					Sharing everything you do<br>
+					I wanna grow old with you<br>
+					<br>
+					Things can come and go I know but<br>
+					Baby I believe<br>
+					Somethings burning strong between us<br>
+					Makes it clear to me<br>
+					<br>
+					I wanna grow old with you<br>
+					I wanna die lying in your arms<br>
+					I wanna grow old with you<br>
+					I wanna be looking in your eyes<br>
+					I wanna be there for you<br>
+					Sharing everything you do<br>
+					I wanna grow old with you<br>
+					Category - Music <br>
+					License - Standard YouTube License<br>
+				</p>
+				</div>
+				</div>
+				<br>				
+				<div class="more">How good is the education being provided by an academic Institution? While there can be sophesticated ways of assessing the quality (e.g. by having peope who understand education and its goals assess the quality by looking at the faculty, courses, method of teaching, etc.) often quality of the overall education is assessed by how well their graduates do in their careers. And an indicator of this is how good a start the graduates get, i.e. the placement record of Institute.</br></br>
+				Most good institutions have good placement records. While good institutions have good placement/careers statistics, there has been a perpetual debate/doubt whether this outcome is due to the fact that these institutes, being higly selective, take in the brightest of the students (if you take the top 5% of the students, of course they will do well – the argument goes), or whether it is due to the value of education they provide.  All of us academics in top institutions believe that while high quality of intake has a role to play, the quality of education provided at these institutions is the key determining factor in the success of these students. Unfortunately, this point cannot be supported by data as it is not possible get the desired data (e.g. average students taken in the top institutions and provided good education and then seeing how they do.)</br></br>
+				At IIIT Delhi, as an Institute which has now evolved into a sought-after, high quality institution, we have a limited data from the first batch to shed some light to this debate. In 2008 the Institute was started – I was appointed the Director (employee #001) in Aug, and we had to start the new session in Sept. As all the admissions were over, the “best” students were already gone – even if they were not, why will they join an Institute that just started and had no faculty or facilities. In any case, we did an entrance test with an eligibility criteria being 60% in class XII.  About 350 students applied, finally around 250 or so appeared, of which we selected 60 – and many of those who were offered did not accept it. Effectively, almost one in every three students who wanted was admitted.   (Now that we have established ourselves as a top Institution,  of those who apply – and only those above 80% in class XII can apply – about 6% are selected.)</br></br>
+				</div>
+				<iframe class="videos" src="//www.youtube.com/embed/Jdi7SU_CMrI" frameborder="0" allowfullscreen></iframe>
+				<p class="video-description">
+					wow:)<br>
+					Category - Music <br>
+					License - Standard YouTube License<br>
+				</p><br>
+				<div class="showmore">
+				<div class="moreblock">
+				<p>
+				So, clearly the first batch was hardly selective by Indian standards. More so, given the fact that admissions in almost all major Institutions were over when we started our process (our exam was held in Aug!).</br></br>
+				After admisison, this batch was taught in their first by guest faculty in borrowed facilites, but using our program and curriculum. In second year, many of the courses were taught by our faculty. It is only by third year when almost all the Computing courses were being taught by our faculty. In other words, whatever is our quality of education now, the first batch did not get it fully.</br></br>
+				Fast forward to 2012, when this batch graduated. The Institute had no track record and no placement history – making it extremely hard to convince companies to visit for campus placement, particularly since we wanted to focus on technology companies as we felt that our students were far too well trained for the “regular” software jobs (by this I mean the entry level jobs offered by the large software houses in the country and where perhaps 90% of the CS graduates in the country get their employment. These jobs, it is well know, have a starting package of about Rs 3 to 3.5 Lac). With effort, we were able to convince some of the companies to come.</br></br>
+				Almost every company that visited, despite their initial doubts, recruited some students. Here we are talking about tech companies that generally have a rigorous selection process – test, multiple rounds of intereviews, etc. Some companies were so happy by the quality of graduating students that they recruited far more than what their best case scenario was (as we were told.) One company, which came for recruiting for their research lab, then called in their other product divisions to recurit. Some highly selective companies which visit few campuses in the country told us that they felt that the graduates are as good as other places they visit.</br></br>
+				The final outcome in numbers: The average salary offer is about Rs 7 Lac (twice that of a regular software job), with majority getting offers of more than Rs 6 Lac, and about 10%  getting offers of more than Rs 10 Lac.</br></br>
+				</p>
+				</div>
+				</div>
+			<iframe class="videos" src="//www.youtube.com/embed/5zE1gBYVaVk?feature=player_detailpage" frameborder="0" allowfullscreen></iframe>
+			<p class="video-description">
+				Download Lorde's album "Pure Heroine (Extended Edition)" featuring "Royals" now:<br>
+				iTunes: <a href="http://smarturl.it/PureHeroineExtended">http://smarturl.it/PureHeroineExtended</a><br>
+				Amazon: <a href="http://smarturl.it/PureHeroineExtendAZ">http://smarturl.it/PureHeroineExtendAZ</a>
+			</p>
+			</div>
+			<div class="author-time-box">
+				<p class="pink"><u><b>Posted By :</b></u></p>
+				<p>YO YO..Honey Singh @ 4:20 pm</p>
+			</div>
+			<div class="MBTFP">
+				<p>M B T F P</p>
+			</div>
+			<div class="Labels">
+				<p class="pink"><b><u>Labels :</b></u></p>
+				<p>IIITD, Placements, Education</p>
+			</div>
+		</div>
+	</body>
+
+</html>
+
+
--- Soumya/index.php
+++ Soumya/index.php
@@ -8,6 +8,47 @@
 		<link rel="stylesheet" href="css/main.css"/>
 		<script type="text/javascript" src="js/jquery.js"></script>
 		<script type="text/javascript" src="js/main.js"></script>
+		<script type="text/javascript">
+		$(function() {
+var showChar = 120, showtxt = "more", hidetxt = "less";
+$('.more').each(function() {
+var content = $(this).text();
+if (content.length > showChar) {
+var con = content.substr(0, showChar);
+var hcon = content.substr(showChar, content.length - showChar);
+var txt= con +  '<span class="dots">...</span><span class="morecontent"><span>' + hcon + '</span>&nbsp;&nbsp;<a href="" class="moretxt">' + showtxt + '</a></span>';
+$(this).html(txt);
+}
+});
+$(".moretxt").click(function() {
+if ($(this).hasClass("sample")) {
+$(this).removeClass("sample");
+$(this).text(showtxt);
+} else {
+$(this).addClass("sample");
+$(this).text(hidetxt);
+}
+$(this).parent().prev().toggle();
+$(this).prev().toggle();
+return false;
+});
+});
+</script>
+<style type="text/css">
+body{
+font-family: Calibri, Arial;
+margin: 0px;
+padding: 0px;
+}
+.more {
+width: 400px;
+background-color: #f0f0f0;
+margin: 10px;
+}
+.morecontent span {
+display: none;
+}
+</style>
 	</head>
 	<body>
 		<div id="container">
@@ -35,6 +76,10 @@
 				</div>
 			</div>
 			<div id="wrapper">
+				<div class="more">How good is the education being provided by an academic Institution? While there can be sophesticated ways of assessing the quality (e.g. by having peope who understand education and its goals assess the quality by looking at the faculty, courses, method of teaching, etc.) often quality of the overall education is assessed by how well their graduates do in their careers. And an indicator of this is how good a start the graduates get, i.e. the placement record of Institute.
+				Most good institutions have good placement records. While good institutions have good placement/careers statistics, there has been a perpetual debate/doubt whether this outcome is due to the fact that these institutes, being higly selective, take in the brightest of the students (if you take the top 5% of the students, of course they will do well – the argument goes), or whether it is due to the value of education they provide.  All of us academics in top institutions believe that while high quality of intake has a role to play, the quality of education provided at these institutions is the key determining factor in the success of these students. Unfortunately, this point cannot be supported by data as it is not possible get the desired data (e.g. average students taken in the top institutions and provided good education and then seeing how they do.)
+				At IIIT Delhi, as an Institute which has now evolved into a sought-after, high quality institution, we have a limited data from the first batch to shed some light to this debate. In 2008 the Institute was started – I was appointed the Director (employee #001) in Aug, and we had to start the new session in Sept. As all the admissions were over, the “best” students were already gone – even if they were not, why will they join an Institute that just started and had no faculty or facilities. In any case, we did an entrance test with an eligibility criteria being 60% in class XII.  About 350 students applied, finally around 250 or so appeared, of which we selected 60 – and many of those who were offered did not accept it. Effectively, almost one in every three students who wanted was admitted.   (Now that we have established ourselves as a top Institution,  of those who apply – and only those above 80% in class XII can apply – about 6% are selected.)
+				</div>
 				<!-- 	wrapper will contain the main article section plus the side bar.
 						So, this in effect wraps the content box and the sidebar. -->
 				<br/><br/><br/>
@@ -53,6 +98,7 @@
 				<p style="padding: 0; margin: 0;">
 				This is supposed to be a very long text somewhere that you should not bother to read anyhow. well that doesn't really matter because I will writ it anyway...so please sutt up.This is supposed to be a very long text somewhere that you should not bother to read anyhow. well that doesn't really matter because I will writ it anyway...so please sutt up.This is supposed to be a very long text somewhere that you should not bother to read anyhow. well that doesn't really matter because I will writ it anyway...so please sutt up.This is supposed to be a very long text somewhere that you should not bother to read anyhow. well that doesn't really matter because I will writ it anyway...so please sutt up.This is supposed to be a very long text somewhere that you should not bother to read anyhow. well that doesn't really matter because I will writ it anyway...so please sutt up.This is supposed to be a very long text somewhere that you should not bother to read anyhow. well that doesn't really matter because I will writ it anyway...so please sutt up.This is supposed to be a very long text somewhere that you should not bother to read anyhow. well that doesn't really matter because I will writ it anyway...so please sutt up.This is supposed to be a very long text somewhere that you should not bother to read anyhow. well that doesn't really matter because I will writ it anyway...so please sutt up.This is supposed to be a very long text somewhere that you should not bother to read anyhow. well that doesn't really matter because I will writ it anyway...so please sutt up.This is supposed to be a very long text somewhere that you should not bother to read anyhow. well that doesn't really matter because I will writ it anyway...so please sutt up.This is supposed to be a very long text somewhere that you should not bother to read anyhow. well that doesn't really matter because I will writ it anyway...so please sutt up.This is supposed to be a very long text somewhere that you should not bother to read anyhow. well that doesn't really matter because I will writ it anyway...so please sutt up.This is supposed to be a very long text somewhere that you should not bother to read anyhow. well that doesn't really matter because I will writ it anyway...so please sutt up.This is supposed to be a very long text somewhere that you should not bother to read anyhow. well that doesn't really matter because I will writ it anyway...so please sutt up.This is supposed to be a very long text somewhere that you should not bother to read anyhow. well that doesn't really matter because I will writ it anyway...so please sutt up.This is supposed to be a very long text somewhere that you should not bother to read anyhow. well that doesn't really matter because I will writ it anyway...so please sutt up.This is supposed to be a very long text somewhere that you should not bother to read anyhow. well that doesn't really matter because I will writ it anyway...so please sutt up.This is supposed to be a very long text somewhere that you should not bother to read anyhow. well that doesn't really matter because I will writ it anyway...so please sutt up.This is supposed to be a very long text somewhere that you should not bother to read anyhow. well that doesn't really matter because I will writ it anyway...so please sutt up.This is supposed to be a very long text somewhere that you should not bother to read anyhow. well that doesn't really matter because I will writ it anyway...so please sutt up.This is supposed to be a very long text somewhere that you should not bother to read anyhow. well that doesn't really matter because I will writ it anyway...so please sutt up.This is supposed to be a very long text somewhere that you should not bother to read anyhow. well that doesn't really matter because I will writ it anyway...so please sutt up.This is supposed to be a very long text somewhere that you should not bother to read anyhow. well that doesn't really matter because I will writ it anyway...so please sutt up.This is supposed to be a very long text somewhere that you should not bother to read anyhow. well that doesn't really matter because I will writ it anyway...so please sutt up.
 				</p>
+				
 				
 			</div>
 			<div id="footer">
