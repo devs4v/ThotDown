6c506c617e66d87a4cdf02b6db2fbf9506ca1187
Anjali <ujjainia.2906@gmail.com>
2014-04-05 13:11:32 +0530

    clean work :D

--- Anjali/blog.php
+++ Anjali/blog.php
@@ -9,61 +9,39 @@
 		<script type="type/javascript" src="js/jquery.scrollTo-1.4.3.1-min.js"></script>
 		<link href='http://fonts.googleapis.com/css?family=Averia+Sans+Libre' rel='stylesheet' type='text/css'>
 		<link href='http://fonts.googleapis.com/css?family=Sigmar+One' rel='stylesheet' type='text/css'>
+		<link rel="stylesheet" href="css/bootstrap.css"/>
 		<link rel="stylesheet" href="css/blog.css"/>
 
 	</head>
 	<body>
 		<div class="post">
 			
-			<div class="post-title">
-				<h1 class="Blog-title">FRIENDS</h1>
-			</div>
+			<div class="post-title"><h1>FRIENDS</h1></div>
 
-			<div class="post-subtitle">
-				<div class="subtitle"> Walking with a friend in the dark is better than walking alone in the light.
-				</div>
+			<div class="post-subtitle">Walking with a friend in the dark is better than walking alone in the light.</div>
 
-				<div class="post-stamp">
-					<p>Thursday, April 3, 2014</p>
-				</div>
-			</div>
+			<div class="post-stamp">Thursday, April 3, 2014</div>
 
-			<div class="post-content">
+			<div class="post-content describe">
 
-				<div class="post-content-text">
-					<img src="pics/frnds2.jpg"/>
-				</div>
-				<div class="describe">
-					“We'll be Friends Forever, won't we, Pooh?' asked Piglet. Even longer,' Pooh answered.”
-				</div>
-				<div class="post-content-text">
-					<img src="pics/frnds1.jpg"/>
-				</div>
-				<div class="describe">
-					“Friendship is the hardest thing in the world to explain. It's not something you learn in school. But if you haven't learned the meaning of friendship, you really haven't learned anything.” </div>
-				<div class="post-content-text">
-					<img src="pics/frnds3.jpg"/>
-				</div>
-				<div class="describe">
-					“Only a true best friend can protect you from your immortal enemies.” 
-				</div>
+				<div class="post-content-text"><img src="pics/frnds2.jpg"/>	</div>
+				“We'll be Friends Forever, won't we, Pooh?' asked Piglet. Even longer,' Pooh answered.”
 				
+				<div class="post-content-text"><img src="pics/frnds1.jpg"/></div>
+				“Friendship is the hardest thing in the world to explain. It's not something you learn in school. But if you haven't learned the meaning of friendship, you really haven't learned anything.” 
+
+				<div class="post-content-text"><img src="pics/frnds3.jpg"/></div>
+				“Only a true best friend can protect you from your immortal enemies.” 
+
 			</div>
 
-			<div class="post-author">
-				<p class="pink"><u><b>Posted By :</b></u></p>
-				<p>YO YO..Honey Singh @ 4:20 pm</p>
+			<div class="post-author">Posted By : <br>YO YO..Honey Singh @ 4:20 pm
 			</div>	
-
-			<div class="post-social">
-				<p>M B T F P</p>
+			<br>
+			<div class="post-social">M B T F P</div>
+			<br>
+			<div class="post-labels">Labels : <p>IIITD, Placements, Education</p>
 			</div>
-
-			<div class="post-labels">
-				<p class="pink"><b><u>Labels :</b></u></p>
-				<p>IIITD, Placements, Education</p>
-			</div>
-
 		</div
 	</body>
 
---
+++ Anjali/mrsite.php
@@ -0,0 +1,23 @@
+<?php /* Sample page */ ?>
+<html>
+	<head>
+		<title>Movie Rating Site</title>
+		<meta charset="UTF-8"/>
+		<meta name="author" content="Shivam Chaturvedi -- The Smarter Techie"/>
+		<meta name="keywords" content="design for the new blogging platform"/>
+		<script type="text/javascript" src="js/jquery.js"></script>
+		<script type="text/javascript" src="js/main.js"></script>
+		<script type="type/javascript" src="js/jquery.scrollTo-1.4.3.1-min.js"></script>
+		<link href='http://fonts.googleapis.com/css?family=Revalia' rel='stylesheet' type='text/css'>
+		<link rel="stylesheet" href="css/bootstrap.css"/>
+		<link rel="stylesheet" href="css/mr.css"/>
+
+	</head>
+	<body>
+		<?php
+		echo "hello";
+		$color= "red";
+		echo "my car is" . $color . "<br>";
+		?>
+	</body>
+</html>
